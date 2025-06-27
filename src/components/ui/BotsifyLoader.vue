825c5a579cabcc80db6fe372668574f76ba790b8
UM4IR AHMAD <um4ir.ahmad@gmail.com>
2025-06-28 00:13:12 +0500

    added Loader in AI Generate Prompt

---
+++ src/components/ui/BotsifyLoader.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="spinner-grow align-self-center"></div>
+</template>
+<style>
+
+@-webkit-keyframes spinner-grow {
+  0% {
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+  50% {
+    opacity: 1;
+  }
+}
+
+@keyframes spinner-grow {
+  0% {
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+  50% {
+    opacity: 1;
+  }
+}
+
+.spinner-grow {
+  display: inline-block;
+  width: 2rem;
+  height: 2rem;
+  vertical-align: text-bottom;
+  background-color: #2e66f4f5;
+  border-radius: 50%;
+  opacity: 0;
+  -webkit-animation: spinner-grow .75s linear infinite;
+  animation: spinner-grow .75s linear infinite;
+}
+</style>
