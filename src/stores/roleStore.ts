2606155ab6b059121fc910850d71927d582e2096
farjadakbar4 <farjadakbar4@gmail.com>
2025-07-30 09:53:52 +0500

    fix: role

--- src/stores/roleStore.ts
+++ src/stores/roleStore.ts
@@ -76,7 +76,7 @@ export const useRoleStore = defineStore('role', () => {
   // Set current user and role
   const setCurrentUser = (user: BotUser) => {
     currentUser.value = user
-    currentRole.value = getRoleFromBotRole(1)
+    currentRole.value = getRoleFromBotRole(user.bot_role)
     userPermissions.value = getPermissionsForRole(currentRole.value)
     
     console.log(`🔐 Role set: ${currentRole.value} (bot_role: ${user.bot_role})`)
