25416860802da6058308c4775be069ec5d5baad1
farjadakbar4 <farjadakbar4@gmail.com>
2025-08-28 20:25:03 +0500

    update plan

--- src/components/ui/UserMenu.vue
+++ src/components/ui/UserMenu.vue
@@ -73,7 +73,7 @@ const openEditProfile = () => {
           <div class="user-details">
             <p class="user-name">{{ currentUser?.name }}</p>
             <p class="user-email">{{ currentUser?.email }}</p>
-            <Badge v-if="currentUser?.subs?.stripe_plan" variant="secondary" size="xs" class="w-fit">{{ currentUser.subs.stripe_plan.replaceAll('-', ' ') }}</Badge>
+            <Badge v-if="currentUser?.subs?.name" variant="secondary" size="xs" class="w-fit">{{ currentUser.subs.name }}</Badge>
           </div>
         </div>
         <div class="dropdown-divider"></div>
